e51cb830fc6412cba32bb5faa823442893d5563a
zeurjr <elizeuribeiroo@gmail.com>
2022-12-25 13:56:18 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "criando imagens"
+
+docker build -t elizeurjr/projeto-backend:1.0 backend/.
+docker build -t elizeurjr/projeto-database:1.0 database/.
+
+echo "realizando o push"
+
+docker push elizeurjr/projeto-backend:1.0
+docker push elizeurjr/projeto-database:1.0
+
+echo "Criando servicos K8s"
+
+kubectl apply -f ./services.yml
+
+echo "Criando deployment"
+
+kubectl apply -f ./deployment.yml
